188d9719a678d87cdc97294a0d48ea2772e7e25b
Airton Franco <airton.franco89@gmail.com>
2025-07-17 16:59:40 -0300

    09 - Comentário

--- src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -1,6 +1,20 @@
 package academy.devdojo.maratonajava.introducao;
 
+/**
+ * isso é um comentario <br>Javadoc<br>
+ * clique CTRL + Q para renderizar o javadoc
+ */
 public class OlaDevDojo {
+    // comentario em linha
+    /*
+    comentario
+    em
+    multiplas linhas
+     */
+    /*
+    Comentarios de codigo não são aconselhaveis em um mundo de denvolvimento real, apenas javadoc
+    se vc esta escrevendo comentarios é porque seu codigo não esta claro o suficiente
+     */
     public static void main (String[] args){
         System.out.println("Ola mundo");
     }
