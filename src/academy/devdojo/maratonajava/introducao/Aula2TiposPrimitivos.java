309f9f4b2f9f698f429b14a40adb45cd9fa4f420
Airton Franco <airton.franco89@gmail.com>
2025-07-18 09:55:14 -0300

    11 - Tipos primitivos pt 02 - declaração e tamanho em memória

--- src/academy/devdojo/maratonajava/introducao/Aula2TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula2TiposPrimitivos.java
@@ -6,6 +6,15 @@ public class Aula2TiposPrimitivos {
             int, double, float, char, byte, short, long, boolean
          */
         int idadeDopai = 36;
+
+        long numeroGrande = 1000000;
+        double salarioDouble = 20000;
+        float salarioFloat = 2500;
+        byte idadeByte = -128; // o baite pod eir de -128 a 127
+        short idadeShort = 10;
+        boolean verdadeiroOrFalse = true;
+        char caractere = 'A'; // here you can use table unicode Ex: char caractere = '\u0041';
+
         System.out.println("A idade é " + idadeDopai + " anos");
     }
 }
